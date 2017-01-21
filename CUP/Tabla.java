754423d70b56ec81dff0147306335f0b6f54bd22
PabloLeon23 <pabloleonalca@gmail.com>
2017-01-21 23:31:36 +0100

    Empezando semantica CUP

---
+++ CUP/Tabla.java
@@ -0,0 +1,27 @@
+import java.util.Hashtable;
+
+class Tabla{
+	public Hashtable<String, String> reservadas = new Hashtable<String, String>();
+	public Tabla(){
+		reservadas.put("require", "REQUIRE");
+		reservadas.put("end", "END");
+		reservadas.put("def", "DEF");
+		reservadas.put("nil","NIL");
+		reservadas.put("true","TRUE");
+		reservadas.put("for","FOR");
+		reservadas.put("break","BREAK");
+		reservadas.put("retry","RETRY");
+		reservadas.put("while","WHILE");
+		reservadas.put("elsif", "ELSIF");
+		reservadas.put("unless","UNLESS");
+		reservadas.put("return","RETURN");
+		reservadas.put("false","FALSE");
+		reservadas.put("if","IF");
+		reservadas.put("then","THEN");
+		reservadas.put("else","ELSE");
+		reservadas.put("and","AND");
+		reservadas.put("or","OR");
+		reservadas.put("not","NOT");
+		reservadas.put("nil","NIL");
+	}
+}
