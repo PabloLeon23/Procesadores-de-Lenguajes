5e31b68735d22e18f01dc454d0b3e41e2be1b3c8
PabloLeon23 <pabloleonalca@gmail.com>
2017-01-25 13:37:41 +0100

    Fin CUP

---
+++ CUP/src/VarInfo.java
@@ -0,0 +1,58 @@
+public class VarInfo{
+	public static final int INT_TYPE=0;
+	public static final int FLOAT_TYPE=1;
+	public static final int STRING_TYPE=2;
+	public static final int BOOL_TYPE=3;
+	public static final int NIL_TYPE=4;
+	public static final int ERROR=5;
+
+	private Object content;
+	private int type;
+
+	public VarInfo(int t,Object cont){
+		content=cont;
+		type=t;
+	}
+
+	public int getType(){
+		return type;
+	}
+	public Object getContent(){
+		return content;
+	}
+
+	public void setType(int t){
+		type=t;
+	}
+	public void setContent(Object cont){
+		content=cont;
+	}
+
+	public String toString(){
+		String ts;
+		if(content==null)
+			ts="";
+		else
+			ts=content.toString();
+		switch(type){
+			case INT_TYPE:
+				ts="int: "+ts;
+				break;
+			case FLOAT_TYPE:
+				ts="float: "+ts;
+				break;
+			case BOOL_TYPE:
+				ts="bool: "+ts;
+				break;
+			case STRING_TYPE:
+				ts="String: \""+ts+"\"";
+				break;
+			case NIL_TYPE:
+				ts="nil: nil";
+				break;
+			default:
+				ts="Error: "+ts;
+		}
+		return ts;
+	}
+}
